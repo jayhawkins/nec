bedb12dc81411e48ad58552ef75adc380df4d99d
John Sanford <johndavidsanford@gmail.com>
2017-07-24 13:11:45 -0400

    file upload per FS#130

--- views/listInsurance.php
+++ views/listInsurance.php
@@ -309,20 +309,37 @@ require '../lib/common.php';
                  <div class="row">
                      <div class="col-sm-12">
                         <label for="fileupload">Policy File Upload</label>
-						<!-- The fileinput-button span is used to style the file input field as button -->
+                        <div class="form-group">
+                        	<!--<input type="file" id="policyFile" name="files[]" class="form-control mb-sm" placeholder="*Policy File" required="required" multiple />-->
+							<span class="btn fileinput-button form-control mb-sm">
+								<!--
+								<i class="glyphicon glyphicon-plus"></i>
+								<span>Add files...</span>
+								-->
+								<!-- The file input field used as target for the file upload widget -->
+								<input id="fileupload" type="file" name="files[]" class="form-control mb-sm" placeholder="*Policy File" required="required" multiple />
+							</span>								<!-- The global progress bar -->
+								<div id="progress" class="progress">
+									<div class="progress-bar progress-bar-success"></div>
+								</div>
+								<!-- The container for the uploaded files -->
+								<div id="files" class="files"></div>
+						</div>
+
+						<!-- The fileinput-button span is used to style the file input field as button --
 						<span class="btn btn-success fileinput-button">
 							<i class="glyphicon glyphicon-plus"></i>
 							<span>Add files...</span>
-							<!-- The file input field used as target for the file upload widget -->
+							<!-- The file input field used as target for the file upload widget --
 							<input id="fileupload" type="file" name="files[]" multiple>
 						</span>
 						<br>
 						<br>
-						<!-- The global progress bar -->
+						<!-- The global progress bar --
 						<div id="progress" class="progress">
 							<div class="progress-bar progress-bar-success"></div>
 						</div>
-						<!-- The container for the uploaded files -->
+						<!-- The container for the uploaded files --
 						<div id="files" class="files"></div>
 						<br>
 						<div class="panel panel-default">
@@ -340,7 +357,7 @@ require '../lib/common.php';
 								</ul>
 							</div>
 						</div>
-
+						-->
 
 
                      </div>
