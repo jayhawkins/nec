8187f8fb4ba69d13a6750a0e4507faba6b076a87
John Sanford <johndavidsanford@gmail.com>
2017-07-24 10:56:30 -0400

    fixing insurance per FS#130

--- views/listInsurance.php
+++ views/listInsurance.php
@@ -380,7 +380,32 @@ require '../lib/common.php';
       </div>
     </div>
 
- <script>
+
+<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
+<!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
+<script src="jQuery-File-Upload-9.18.0/js/vendor/jquery.ui.widget.js"></script>
+<!-- The Load Image plugin is included for the preview images and image resizing functionality -->
+<script src="//blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js"></script>
+<!-- The Canvas to Blob plugin is included for image resizing functionality -->
+<script src="//blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
+<!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
+<script src="jQuery-File-Upload-9.18.0/js/jquery.iframe-transport.js"></script>
+<!-- The basic File Upload plugin -->
+<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload.js"></script>
+<!-- The File Upload processing plugin -->
+<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload-process.js"></script>
+<!-- The File Upload image preview & resize plugin -->
+<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload-image.js"></script>
+<!-- The File Upload audio preview plugin -->
+<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload-audio.js"></script>
+<!-- The File Upload video preview plugin -->
+<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload-video.js"></script>
+<!-- The File Upload validation plugin -->
+<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload-validate.js"></script>
+<script>
+/*jslint unparam: true, regexp: true */
 
     loadTableAJAX();
 
@@ -427,32 +452,6 @@ require '../lib/common.php';
 
     } );
 
-
-<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
-<!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
-<script src="jQuery-File-Upload-9.18.0/js/vendor/jquery.ui.widget.js"></script>
-<!-- The Load Image plugin is included for the preview images and image resizing functionality -->
-<script src="//blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js"></script>
-<!-- The Canvas to Blob plugin is included for image resizing functionality -->
-<script src="//blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
-<!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
-<script src="jQuery-File-Upload-9.18.0/js/jquery.iframe-transport.js"></script>
-<!-- The basic File Upload plugin -->
-<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload.js"></script>
-<!-- The File Upload processing plugin -->
-<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload-process.js"></script>
-<!-- The File Upload image preview & resize plugin -->
-<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload-image.js"></script>
-<!-- The File Upload audio preview plugin -->
-<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload-audio.js"></script>
-<!-- The File Upload video preview plugin -->
-<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload-video.js"></script>
-<!-- The File Upload validation plugin -->
-<script src="jQuery-File-Upload-9.18.0/js/jquery.fileupload-validate.js"></script>
-<script>
-/*jslint unparam: true, regexp: true */
 /*global window, $ */
 $(function () {
     'use strict';
