d6284f190c86d52a54c811988b49e070bef98cd6
John Sanford <johndavidsanford@gmail.com>
2017-07-24 12:02:07 -0400

    file upload per FS#130

--- views/listInsurance.php
+++ views/listInsurance.php
@@ -306,9 +306,7 @@ require '../lib/common.php';
                         	<!--<input type="file" id="policyFile" name="files[]" class="form-control mb-sm" placeholder="*Policy File" required="required" multiple />-->
 							<span class="btn btn-success fileinput-button">
 								<i class="glyphicon glyphicon-plus"></i>
-								<!--
 								<span>Add files...</span>
-								-->
 								<!-- The file input field used as target for the file upload widget -->
 								<input id="fileupload" type="file" name="files[]" class="form-control mb-sm" placeholder="*Policy File" required="required"multiple />
 								<!-- The global progress bar -->
