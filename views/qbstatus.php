e59fa206426fba936d392bea186d9722260ea9d0
Jay Hawkins <jaycarl.hawkins@gmail.com>
2017-08-21 10:44:49 -0400

    Resolved issue with qbstatus.php using correct header configuration

--- views/qbstatus.php
+++ views/qbstatus.php
@@ -1,5 +1,6 @@
 <?php
 
+
 session_start();
 
 require '../../nec_config.php';
@@ -10,54 +11,81 @@ define('OAUTH_REQUEST_URL', 'https://oauth.intuit.com/oauth/v1/get_request_token
 define('OAUTH_ACCESS_URL', 'https://oauth.intuit.com/oauth/v1/get_access_token');
 define('OAUTH_AUTHORISE_URL', 'https://appcenter.intuit.com/Connect/Begin');
 // The url to this page. it needs to be dynamic to handle runnable's dynamic urls
-define('CALLBACK_URL','http://nec.dubtel.com/qb_api_status');
+define('CALLBACK_URL', HTTP_HOST . '/qb_api_status');
 // cleans out the token variable if comming from
 // connect to QuickBooks button
 if ( isset($_GET['start'] ) ) {
   unset($_SESSION['token']);
 }
- 
+
 try {
   $oauth = new OAuth( OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_URI);
   $oauth->enableDebug();
   $oauth->disableSSLChecks(); //To avoid the error: (Peer certificate cannot be authenticated with given CA certificates)
+
   if (!isset( $_GET['oauth_token'] ) && !isset($_SESSION['token']) ){
+
 	// step 1: get request token from Intuit
     $request_token = $oauth->getRequestToken( OAUTH_REQUEST_URL, CALLBACK_URL );
+
 		$_SESSION['secret'] = $request_token['oauth_token_secret'];
-		// step 2: send user to intuit to authorize 
-		header('Location: '. OAUTH_AUTHORISE_URL .'?oauth_token='.$request_token['oauth_token']);
+        try {
+            // step 2: send user to intuit to authorize
+            //header('Location: '. OAUTH_AUTHORISE_URL .'?oauth_token='.$request_token['oauth_token']);
+            // Create a stream
+            // Look at historical data
+            $args = array(
+                "oauth_token"=>$request_token['oauth_token']
+            );
+            $url = OAUTH_AUTHORISE_URL . "?".http_build_query($args);
+            $options = array(
+                'http' => array(
+                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                    'method'  => 'GET'
+                )
+            );
+            $context  = stream_context_create($options);
+            $result = json_decode(file_get_contents($url,false,$context),true);
+            echo $result;
+        } catch(OAuthException $e) {
+            echo "Got auth exception";
+            echo '<pre>';
+            print_r($e);
+            die();
+        }
+
 	}
-	
+
 	if ( isset($_GET['oauth_token']) && isset($_GET['oauth_verifier']) ){
 		// step 3: request a access token from Intuit
     $oauth->setToken($_GET['oauth_token'], $_SESSION['secret']);
 		$access_token = $oauth->getAccessToken( OAUTH_ACCESS_URL );
-		
+
 		$_SESSION['token'] = serialize( $access_token );
     $_SESSION['realmId'] = '193514475422329';  // realmId is legacy for customerId
     //$_SESSION['dataSource'] = $_REQUEST['dataSource'];
-	
+
 	 $token = $_SESSION['token'] ;
 	 $realmId = $_SESSION['realmId'];
 	 //$dataSource = $_SESSION['dataSource'];
 	 $secret = $_SESSION['secret'] ;
-	 
+
     // write JS to pup up to refresh parent and close popup
     echo '<script type="text/javascript">
             window.opener.location.href = window.opener.location.href;
             window.close();
           </script>';
   }
- 
+
 } catch(OAuthException $e) {
 	echo "Got auth exception";
 	echo '<pre>';
 	print_r($e);
 }
+
 ?>
 
- 
+
 
  <script src="vendor/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
 
@@ -129,10 +157,10 @@ try {
      </header>
      <div class="widget-body">
          <br /><br />
-         
+
      </div>
  </section>
- 
+
 
   <!-- Modal -->
 
