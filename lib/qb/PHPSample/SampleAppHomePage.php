c9d4c91b17316b3a8c7c048a774d34b49c8ed0b5
ygtandoh <ygtandoh@192.168.200.202>
2017-08-21 15:23:24 -0400

    NEC QB integration

--- lib/qb/PHPSample/SampleAppHomePage.php
+++ lib/qb/PHPSample/SampleAppHomePage.php
@@ -12,14 +12,14 @@
 </head>
 <body>
 
-<a href="http://localhost/PHPSample/index.php?connectWithIntuitOpenId">Go Back</a>&nbsp;&nbsp;&nbsp;
+<a href="index.php?connectWithIntuitOpenId">Go Back</a>&nbsp;&nbsp;&nbsp;
 
 <?php
  # Add a link to allow the user to logout. The link makes a JavaScript call to intuit.ipp.anywhere.logout()
- echo '<a href="javascript:void(0)" onclick="return intuit.ipp.anywhere.logout(function () { window.location.href = \'http://localhost/PHPSample/index.php\'; });">Sign Out</a>&nbsp;&nbsp;&nbsp;';
+ echo '<a href="javascript:void(0)" onclick="return intuit.ipp.anywhere.logout(function () { window.location.href = \'index.php\'; });">Sign Out</a>&nbsp;&nbsp;&nbsp;';
 ?>
 			
-<a target="_blank" href="http://localhost/PHPSample/ReadMe.htm">Read Me</a><br />
+<a target="_blank" href="ReadMe.htm">Read Me</a><br />
 
 <button class="myButton" style="margin-top:30px;margin-bottom:30px" title="Create an employee in QBO" onclick='createEmployee()'>Create Employee </button> <br />
 
@@ -32,19 +32,19 @@
 
 <script>
 function createEmployee(){
-window.location.href = "http://localhost/PHPSample/EmployeeCreate.php";
+window.location.href = "EmployeeCreate.php";
 }
 
 function getAccounts(){
-window.location.href = "http://localhost/PHPSample/AccountsFindAll.php";
+window.location.href = "AccountsFindAll.php";
 }
 
 function createGJE(){
-window.location.href = "http://localhost/PHPSample/CreateGJE.php";
+window.location.href = "CreateGJE.php";
 }
 
 function showTrialBalanceReport(){
-window.location.href = "http://localhost/PHPSample/TrailBalanceReport.php";
+window.location.href = "TrailBalanceReport.php";
 }
 
 </script>
