d5dffd2ce3d077bfec1cb698794448028a82354d
ygtandoh <ygtandoh@192.168.200.202>
2017-08-21 05:38:51 -0400

    Quickbooks api connect Feature for Nec

---
+++ lib/quickbooksconfig.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define('OAUTH_CONSUMER_KEY', 'qyprd3v8NFl7W89u9z1rvgIsxSu6zo');
+  define('OAUTH_CONSUMER_SECRET', 'NQzoaHCCRB3vXqTzYZ4GYE2OTtPFbP5lh3zhxYMb');
+ ?> 
+
+
