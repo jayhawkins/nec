247c03e1facbadf612156c782833add655e8b262
ygtandoh <ygtandoh@192.168.200.202>
2017-08-23 18:50:40 -0400

    Fix Working Deals Report

--- QBO/src/Pages/CustomerCreate.php
+++ QBO/src/Pages/CustomerCreate.php
@@ -126,10 +126,35 @@ $vendorObj->CompanyName = $carrier_name;
 
 try{
  $resultVendorObj = $dataService->Add($vendorObj);
+ $vendorid = $resultVendorObj->id;
  print_r($resultVendorObj); 
 } catch (Exception $e){
  echo $e->getMessage();
 }
+
+$purchaseorderObject = new IPPPurchaseOrder();
+$purchaseorderObject->Memo = $customer_notes;
+$purchaseorderObject->VendorRef =  intval($vendorid);
+$Line2 = new IPPline();
+$Line2->Amount = floatval($customer_rate);
+$Line2->DetailType = 'SalesItemLineDetail';
+$Line2->Description = $customer_notes;
+ 
+$saleItemLineDetail2 = new IPPSalesItemLineDetail();
+$saleItemLineDetail2->ItemRef = 1;
+$Line2->SalesItemLineDetail = $saleItemLineDetail2;
+
+$purchaseorderObject->Line = $Line2;
+
+try{
+ $resultingPoObj = $dataService->Add($purchaseorderObject);
+ print_r($resultingPoObj); 
+} catch (Exception $e){
+ echo $e->getMessage();
+}
+
+
+
 }
 
 exit();
