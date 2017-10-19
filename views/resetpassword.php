7cb5eca01d0b08198b2fbf3a1efcab177d8a206b
Jay Hawkins <jaycarl.hawkins@gmail.com>
2017-10-19 17:23:21 -0400

    FS393 Resolve issue with stylesheets and js not being included in the page

--- views/resetpassword.php
+++ views/resetpassword.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>NEC - Reset Password</title>
-    <link href="<?php echo HTTP_HOST ?>/css/application.min.css" rel="stylesheet">
+    <link href="<?php echo HTTP_HOST; ?>/css/application.min.css" rel="stylesheet">
     <!-- as of IE9 cannot parse css files with more that 4K classes separating in two files -->
     <!--[if IE 9]>
         <link href="css/application-ie9-part2.css" rel="stylesheet">
@@ -41,7 +41,7 @@
         <div class="row">
             <div class="col-xl-4 col-md-6 col-xs-10 col-xl-offset-4 col-md-offset-3 col-xs-offset-1">
                 <h5 class="widget-login-logo animated fadeInUp">
-                    <img src="<?php echo HTTP_HOST ?>/img/nec_logo.png" />
+                    <img src="<?php echo HTTP_HOST; ?>/img/nec_logo.png" />
                 </h5>
                 <section class="widget widget-login animated fadeInUp">
                     <header>
@@ -89,24 +89,24 @@
 </div>
 
 <!-- common libraries. required for every page-->
-<script src="<?php echo HTTP_HOST ?>/vendor/jquery/dist/jquery.min.js"></script>
-<script src="<?php echo HTTP_HOST ?>/vendor/jquery-pjax/jquery.pjax.js"></script>
-<script src="<?php echo HTTP_HOST ?>/vendor/tether/dist/js/tether.js"></script>
-<script src="<?php echo HTTP_HOST ?>/vendor/bootstrap/js/dist/util.js"></script>
-<script src="<?php echo HTTP_HOST ?>/vendor/bootstrap/js/dist/collapse.js"></script>
-<script src="<?php echo HTTP_HOST ?>/vendor/bootstrap/js/dist/dropdown.js"></script>
-<script src="<?php echo HTTP_HOST ?>/vendor/bootstrap/js/dist/button.js"></script>
-<script src="<?php echo HTTP_HOST ?>/vendor/bootstrap/js/dist/tooltip.js"></script>
-<script src="<?php echo HTTP_HOST ?>/vendor/bootstrap/js/dist/alert.js"></script>
-<script src="<?php echo HTTP_HOST ?>/vendor/slimScroll/jquery.slimscroll.min.js"></script>
-<script src="<?php echo HTTP_HOST ?>/vendor/widgster/widgster.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/jquery-pjax/jquery.pjax.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/tether/dist/js/tether.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/bootstrap/js/dist/util.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/bootstrap/js/dist/collapse.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/bootstrap/js/dist/dropdown.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/bootstrap/js/dist/button.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/bootstrap/js/dist/tooltip.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/bootstrap/js/dist/alert.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/widgster/widgster.js"></script>
 
 <!-- common app js -->
-<script src="<?php echo HTTP_HOST ?>/js/settings.js"></script>
-<script src="<?php echo HTTP_HOST ?>/js/app.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/js/settings.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/js/app.js"></script>
 
 <!-- page specific libs -->
-<script src="<?php echo HTTP_HOST ?>/vendor/parsleyjs/dist/parsley.min.js"></script>
+<script src="<?php echo HTTP_HOST; ?>/vendor/parsleyjs/dist/parsley.min.js"></script>
 <!-- page specific js -->
 </body>
 </html>
