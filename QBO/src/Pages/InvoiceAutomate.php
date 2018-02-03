4a25026a673c91ad2b59df07575a755f536dad67
yawtandoh <yawtandoh@192.168.200.202>
2018-02-03 06:01:31 -0500

    Invoice Automation Script for NEC

---
+++ QBO/src/Pages/InvoiceAutomate.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* replace things with { } curly brackets with the appropriate information
+* including the curly brackets! don't leave those in there...
+* You'll use your database handler ($dbh) when you run your queries
+**/
+
+//connect to the server
+
+
+
+$dbh = mysqli_connect("localhost", "root", "pqlamz", "nec")
+     or die ('cannot connect to database because ' . mysqli_connect_error());
+	 
+
+
+//select from orders that have not been invoiced
+//see joins
+	 
+	 //run the query
+$loop = mysqli_query($dbh, "SELECT p.id,p.customerID,p.cost, p.orderID, p.orderDetailID,d.originationCity,d.originationState,d.destinationCity,d.destinationState ,e.name,l.address1,l.city,l.state,l.zip FROM nec.approved_pod p join order_details d on p.orderDetailID = d.id join entities e on p.customerID = e.id join locations l on e.id = l.entityID where l.locationTypeID = 1 and p.hasBeenInvoiced = 0")
+   or die (mysqli_error($dbh));
+
+while ($row = mysqli_fetch_array($loop))
+{
+     echo $row['id'] . " " .echo $row['orderID'] . " " . $row['originationCity'] . " " . $row['originationState'] . " " . $row['destinationCity'] . " " . $row['destinationState'] . " " . $row['name'] . " " . $row['address1']." " . $row['city']." " . $row['state'] ." " . $row['zip'].   "<br/>";
+}
+	 
+?>
