5803a9d8090144262fc4ada282f929218a3aad4d
dennis <dennis@dennis-HP-ENVY-Laptop-17-ae1xx>
2018-02-23 16:35:18 -0500

    FS#726 - QuickBooks Product/Service Column
    Change Product/Service Column in QuickBooks damage claims to say; “Damage Claims”

--- QuickBooks/src/pages/test.php
+++ QuickBooks/src/pages/test.php
@@ -14,40 +14,43 @@ use QuickBooksOnline\API\Facades\Customer;
 use QuickBooksOnline\API\Facades\Invoice;
 
 
-    
-    //query for cutomer// Prep Data Services
-    $dataService = DataService::Configure(array(
-           'auth_mode' => 'oauth2',
-             'ClientID' => "Q0bCkjuFuWa8MxjEDqYenaCreMUZjyAJ2UyNhnOmdVGEDNkkkD",
-             'ClientSecret' => "ahfR70aIvIatES37ZeoJztAJx7Ki1PvoGhfNVTja",
-        'accessTokenKey' =>  "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..9yIB9pjdryM3QDYIZtqV-A.EhLOo88RDDLpYJh5Q8FRvW0vh_KhsS2_iQXGRYE30kR8lx9QxAT5y18-wXjmbA7V-AVqzBJyKhshxUUmlTETIdkLIAy-hIHevMNaZfDuqqFYfV2Vm5DXrg8JnhsLmRqGxKPLkfeR38v3kdF0golrMk0llkAZuNqUlekXkqtJo4mB72TRZ2B-iDtK0cPoO0KcqG_0DbJzV_kH104De5kMOZuGMxlIa-o_rHfWVdPbrhxtDsrHxVLgNmgg7jCCxLVJuwS0fNgKxi9bQ2OPXYM6wij0BRtLeiSzDdl7xTk6d81X_f2lH_Nlh8jTHSiamSeyl7dsfzHZZn8HNYxzQWdKlU9TrIxHr4tX9v9gtkL-IgnManTfxYFzyuUzbLFlGwcV9Ef32iFWGmbbrzHsyH5ByedAQBNDU0qA44otvokBQoFwdXYIhHsCwzRQvnfKM1vlTdAbuFH2dN3IYcxsSFF19Nrf-IFgNZ9FOkP1F1lgavpdEiG8PVw_AtXhOScjjpKFOdCM6KI1IMKgUTRtGVkh-uWm9Fzh7RnMRjKibcLnOJjIGg8KQAu1h2D9JUh-px5L3acg-cU3X8GtxqOWAyNakAkGPcZyisPRS0WZ8Z0qnJo_qSQxvPwZ2xgnqlvZxBOGTwOOQvsU1FeK-0g1YuSzSjLA_qkJSVKPAsQ_qYMBmb0bUtYUM34KHdT1Uyz2Rmnz.cufIfMnBktHRjkRvTa4lKA",
-        'refreshTokenKey' => "L0115281440892w0kOQQPwOoR8QCd2BTH4akrsRUMLrG1Lof19",
-              'QBORealmID' => "123145985783569",
-             'baseUrl' => "https://sandbox-quickbooks.api.intuit.com"
-    ));
+$dbh = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME)
+     or die ('cannot connect to database because ' . mysqli_connect_error());
 
-    $item = $dataService->Query("SELECT * FROM Item where name = 'Damage Claims'");
-    $error = $dataService->getLastError();
+
+// Run the query
+$orderLoop = mysqli_query($dbh, "select distinct(a.orderID), count(a.vinNumber) as totalApproved, b.qty from approved_pod a, orders b where a.hasBeenInvoiced=1 and b.id=a.orderID and b.status='Open' group by a.orderID, a.vinNumber")
+   or die (mysqli_error($dbh));
+
+
+while($orderRow = mysqli_fetch_array($orderLoop)){
     
-    if ($error != null) {
-        echo "The Status code is: " . $error->getHttpStatusCode() . "<br><br>";
-        echo "The Helper message is: " . $error->getOAuthHelperError() . "<br><br>";
-        echo "The Response message is: " . $error->getResponseBody() . "<br><br>";
-        exit();
-    }
-    else{
+    $orderID = $orderRow['orderID']; 
+    $totalInvoiced = $orderRow['totalApproved']; 
+    $qty = $orderRow['qty']; 
+    
+    // Was all the trailers invoiced?
+    if($totalInvoiced >= $qty){
+        // Yes,
+        // Close the order
         
-        print_r(count($item));
-        exit();
-        
-        foreach($item as $key => $value){
-            
-            print_r("ID: ". $value->Id);
-            echo "<br>";
-            print_r("Name: ". $value->Name);
-            echo "<hr>";
+        // Create connection
+        $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        $sql = "UPDATE orders SET status='Closed', updatedAt = NOW() WHERE id=".$orderID;
+        echo $sql;
+        if ($conn->query($sql) === TRUE) {
+            echo "Record updated successfully";
+        } else {
+            echo "Error updating record: " . $conn->error;
         }
+
+        $conn->close();
     }
     
-   
+}
 ?>
