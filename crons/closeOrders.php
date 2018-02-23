7fdc5797e23c9346eaf011a387f1d3fdb13ba099
dennis <dennis@dennis-HP-ENVY-Laptop-17-ae1xx>
2018-02-23 16:38:12 -0500

    FS#501 - Order - Create workflow to complete the order NEC Admin
    Once all PODs have been completed we need to complete the back-end functionality to close the loop.
    
    When the Final Carrier or NEC Admin uploads the final POD for their relay.
    - A notifications should be sent to the NEC Admin to team to advise the order is complete.
    - The NEC Admin should receive an indicator in the application that the order is ready to be complete.
    - The NEC Admin should be able to select the final POD and send to Quickbooks to attach to the Quickbooks invoice to send to the Customer/Carrier
    - The order should be marked complete and closed.
    - The order should no longer appear in the Order section
    - The order should be moved to Invoice section of the application.
    
    - The NEC Admin, Customer and Carrier should be able to view both Open and Closed orders.

---
+++ crons/closeOrders.php
@@ -0,0 +1,47 @@
+<?php
+//Replace the line with require "vendor/autoload.php" if you are using the Samples from outside of _Samples folder
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require '../../../../nec_config.php';
+
+$dbh = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME)
+     or die ('cannot connect to database because ' . mysqli_connect_error());
+
+
+// Run the query
+$orderLoop = mysqli_query($dbh, "select distinct(a.orderID), count(a.vinNumber) as totalApproved, b.qty from approved_pod a, orders b where a.hasBeenInvoiced=1 and b.id=a.orderID and b.status='Open' group by a.orderID, a.vinNumber")
+   or die (mysqli_error($dbh));
+
+
+while($orderRow = mysqli_fetch_array($orderLoop)){
+    
+    $orderID = $orderRow['orderID']; 
+    $totalInvoiced = $orderRow['totalApproved']; 
+    $qty = $orderRow['qty']; 
+    
+    // Was all the trailers invoiced?
+    if($totalInvoiced >= $qty){
+        // Yes,
+        // Close the order
+        
+        // Create connection
+        $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        $sql = "UPDATE orders SET status='Closed', updatedAt = NOW() WHERE id=".$orderID;
+        echo $sql;
+        if ($conn->query($sql) === TRUE) {
+            echo "Record updated successfully";
+        } else {
+            echo "Error updating record: " . $conn->error;
+        }
+
+        $conn->close();
+    }
+    
+}
+?>
