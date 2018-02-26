e0796435a048c70de18e3c2224afdc620c3e78a3
yg_ta <yg_ta@192.168.200.198>
2018-02-26 12:55:31 -0500

    Fix View

--- QuickBooks/src/pages/refreshTokenSample.php
+++ QuickBooks/src/pages/refreshTokenSample.php
@@ -2,6 +2,7 @@
 //header('Content-type: application/xml');
 //Replace the line with require "vendor/autoload.php" if you are using the Samples from outside of _Samples folder
 include('../config.php');
+require '../../../../nec_config.php';
 
 
 
@@ -10,13 +11,39 @@ use QuickBooksOnline\API\DataService\DataService;
 use QuickBooksOnline\API\PlatformService\PlatformService;
 use QuickBooksOnline\API\Core\Http\Serialization\XmlObjectSerializer;
 
+
+$ClientID = ""; 
+$ClientSecret = "";
+$accessTokenKey = "";
+$refreshTokenKey = "";
+$QBORealmID = "";
+$dbh = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME)
+     or die ('cannot connect to database because ' . mysqli_connect_error());
+
+
+//get quickbooks credentials from db
+   //run the query
+$qbLoop = mysqli_query($dbh, "select * from quickbooks_authentication where id = 1")
+   or die (mysqli_error($dbh));
+
+
+while($qbRow = mysqli_fetch_array($qbLoop)){
+    
+    $ClientID = $qbRow['clientID']; 
+$ClientSecret = $qbRow['ClientSecret']; 
+$accessTokenKey = $qbRow['accessTokenKey']; 
+$refreshTokenKey = $qbRow['refreshToken']; 
+$QBORealmID =  $qbRow['realmID']; 
+    
+}
+
 $dataService = DataService::Configure(array(
     'auth_mode' => 'oauth2',
-    'ClientID' => "Q0bCkjuFuWa8MxjEDqYenaCreMUZjyAJ2UyNhnOmdVGEDNkkkD",
-    'ClientSecret' => "ahfR70aIvIatES37ZeoJztAJx7Ki1PvoGhfNVTja",
-    'accessTokenKey' =>  "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..ZzdmZi1Dbxm4C-UxGnU65A.oQdIxMAuLuX26eDkLe3l_5IIh7hy4I2PRu5dGEID7HRPaUvi5c4uy8FU3ykKzz6OXAxQBOmc0Mi2vdU1aGqO8ygmcFXOgoHaPftotNxzipq6f0kBSB0FCnv9-kN46jW7whuNa_eJBOzzQqn20n8LKq5ORo29RqMxM6qA4eJ7EGyY9lCia5wyPlM44csCbGGoL4BbwD8i3oQM23qgvSWy07X30pUFFv2neA_DqfsArrM-m8vMHm4hyuiQbpU1Ahnb4zJcFlQRE3dHYS7ZfE4XrwsWEsKa6HDodxFDY52yDd5GUqNNDMljnUXjUF1yMytvCethe1K0T6PXG9S2ekQWhf2dRsmk9pF82St938xNKgc-We4Mc7Uu2caeIUuDHlOdR6XszLnD_804WbdStDVORcjMQDRV1ij-cE8o3N9oOM8ZakyivnyUGYnXM_UUt0YSMFUiV5SX--O2J73yTLApMpFgRRuurVRbQHQ1X_Qc8BWODn8gjmdYVc71IUUyIi4CZA6E6DK_T0QUTdChcget4Oq-S7sNz5ijMNnX3y2FF3xItJ4aHehkZy1uq5ncLDEmU0-4-RO1voJ9wFG2wl2J-slWKcNi0krwXnzdCx99jm5WbzS1BwnQQnnGl8VbHY0Rp_tX74-x3j7ymdN_QIV3lVUHATDK2ogIqCbfVfapvi9yu5CwVOQToWxipcsWc13Y.ZJoG7VRPh17F7jxRmgIJ3Q",
-    'refreshTokenKey' => "Q011528393690gmnRRyqs04lrJ5vj3wcdKLVitLlwsAqDK3888",
-    'QBORealmID' => "123145985783569",
+    'ClientID' => $ClientID,
+             'ClientSecret' => $ClientSecret,
+    'accessTokenKey' =>  $accessTokenKey,
+    'refreshTokenKey' => $refreshTokenKey,
+              'QBORealmID' => $QBORealmID,
     'baseUrl' => "https://sandbox-quickbooks.api.intuit.com"
 ));
 
