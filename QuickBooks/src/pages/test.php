86cc5b1617dfc69269c62074d41131e6d4c098f0
dennis <dennis@dennis-HP-ENVY-Laptop-17-ae1xx>
2018-02-28 11:55:44 -0500

    FS#739 - QuickBooks - Generate New Authentication token
    We will need to generate a new set of tokens so that the session is constantly alive.

--- QuickBooks/src/pages/test.php
+++ QuickBooks/src/pages/test.php
@@ -2,46 +2,55 @@
 //Replace the line with require "vendor/autoload.php" if you are using the Samples from outside of _Samples folder
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+include('../config.php');
 
 require '../../../../nec_config.php';
 
+
+use QuickBooksOnline\API\Core\ServiceContext;
+use QuickBooksOnline\API\DataService\DataService;
+use QuickBooksOnline\API\PlatformService\PlatformService;
+use QuickBooksOnline\API\Core\Http\Serialization\XmlObjectSerializer;
+
+
+$ClientID = ""; 
+$ClientSecret = "";
+$accessTokenKey = "";
+$refreshTokenKey = "";
+$QBORealmID = "";
 $dbh = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME)
      or die ('cannot connect to database because ' . mysqli_connect_error());
 
 
-// Run the query
-$orderLoop = mysqli_query($dbh, "select distinct(a.orderID), count(a.vinNumber) as totalApproved, b.qty from approved_pod a, orders b where a.hasBeenInvoiced=1 and b.id=a.orderID and b.status='Open' group by a.orderID, a.vinNumber")
+//get quickbooks credentials from db
+   //run the query
+$qbLoop = mysqli_query($dbh, "select * from quickbooks_authentication where id = 1")
    or die (mysqli_error($dbh));
 
 
-while($orderRow = mysqli_fetch_array($orderLoop)){
+while($qbRow = mysqli_fetch_array($qbLoop)){
     
-    $orderID = $orderRow['orderID']; 
-    $totalInvoiced = $orderRow['totalApproved']; 
-    $qty = $orderRow['qty']; 
-    
-    // Was all the trailers invoiced?
-    if($totalInvoiced >= $qty){
-        // Yes,
-        // Close the order
-        
-        // Create connection
-        $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-
-        $sql = "UPDATE orders SET status='Closed', updatedAt = NOW() WHERE id=".$orderID;
-        echo $sql;
-        if ($conn->query($sql) === TRUE) {
-            echo "Record updated successfully";
-        } else {
-            echo "Error updating record: " . $conn->error;
-        }
-
-        $conn->close();
-    }
+$ClientID = $qbRow['clientID']; 
+$ClientSecret = $qbRow['ClientSecret']; 
+$accessTokenKey = $qbRow['accessTokenKey']; 
+$refreshTokenKey = $qbRow['refreshToken']; 
+$QBORealmID =  $qbRow['realmID']; 
     
 }
+
+$dataService = DataService::Configure(array(
+    'auth_mode' => 'oauth2',
+    'ClientID' => $ClientID,
+    'ClientSecret' => $ClientSecret,
+    'scope' => "com.intuit.quickbooks.accounting",
+    'QBORealmID' => $QBORealmID,
+    'baseUrl' => "https://sandbox-quickbooks.api.intuit.com"
+));
+
+$OAuth2LoginHelper = $dataService->getOAuth2LoginHelper();
+
+$authorizationUrl = $OAuth2LoginHelper->getAuthorizationCodeURL();
+
+print_r($authorizationUrl);
+
 ?>
