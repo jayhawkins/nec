233e158f4a23cc139a9344753ca8b589e22b1ae3
dennis <dennis@dennis-HP-ENVY-Laptop-17-ae1xx>
2018-02-28 12:01:27 -0500

    FS#739 - QuickBooks - Generate New Authentication token
    We will need to generate a new set of tokens so that the session is constantly alive.

--- QuickBooks/src/pages/test.php
+++ QuickBooks/src/pages/test.php
@@ -48,10 +48,17 @@ $dataService = DataService::Configure(array(
 ));
 
 $OAuth2LoginHelper = $dataService->getOAuth2LoginHelper();
+$error = $dataService->getLastError();
+if ($error != null) {
+    echo "The Status code is: " . $error->getHttpStatusCode() . "\n";
+    echo "The Helper message is: " . $error->getOAuthHelperError() . "\n";
+    echo "The Response message is: " . $error->getResponseBody() . "\n";
+} else {
+    
+    $authorizationUrl = $OAuth2LoginHelper->getAuthorizationCodeURL();
 
-$authorizationUrl = $OAuth2LoginHelper->getAuthorizationCodeURL();
-
-print_r($authorizationUrl);
+    print_r($authorizationUrl);
 exit();
+}
 
 ?>
